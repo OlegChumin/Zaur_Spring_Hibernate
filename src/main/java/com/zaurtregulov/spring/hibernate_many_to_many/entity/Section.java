6214a1a60ce8fb47c5ade6d8cb59cea6a98783b3
Oleg <tschumin.oleg@gmail.com>
2023-05-02 11:52:40 +0300

    Add new files

---
+++ src/main/java/com/zaurtregulov/spring/hibernate_many_to_many/entity/Section.java
@@ -0,0 +1,9 @@
+package com.zaurtregulov.spring.hibernate_many_to_many.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "")
+public class Section {
+}
