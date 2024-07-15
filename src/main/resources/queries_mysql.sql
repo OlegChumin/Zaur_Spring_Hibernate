856f1fe1228ae8441a7aadfea76f76e0ada6568b
Oleg Chumin <tschumin.oleg@gmail.com>
2024-07-15 07:22:12 +0300

    Add docker config

---
+++ src/main/resources/queries_mysql.sql
@@ -0,0 +1,11 @@
+CREATE TABLE employees (
+                           id INT AUTO_INCREMENT PRIMARY KEY,
+                           name VARCHAR(100),
+                           age INT,
+                           position VARCHAR(100)
+);
+
+INSERT INTO employee_db.employees (name, age, position) VALUES ('John Doe', 35, 'developer');
+
+
+
